96209d23022c7ed3383c727676c6ca2915dcf4b5
fabionunesdev <fabionunesbr@gmail.com>
2022-12-11 09:19:08 -0300

    Seção 2 - Adicionada novas entidades
    Adicionada classe Lanche e Categoria com seus relacionamentos.

---
+++ LanchesMac/LanchesMac/Models/Lanche.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
+
+namespace LanchesMac.Models;
+
+public class Lanche
+{
+    public int LancheId { get; set; }
+    public string Nome { get; set; }
+    public string DescricaoCurta { get; set; }
+    public string DescricaoDetalhada { get; set; }
+    public decimal Preco { get; set; }
+    public string ImagemUrl { get; set; }
+    public string ImagemThumbnailUrl { get; set; }
+    public bool IsLanchePreferido { get; set; }
+    public bool EmEstoque { get; set; }
+
+    // Relacionamento com Categoria
+    public int CategoriaId { get; set; }
+    public virtual Categoria Categoria { get; set; }
+}
