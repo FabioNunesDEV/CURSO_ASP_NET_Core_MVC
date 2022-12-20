ab2be4c26c31c5af6c0be3b26df3fb19256da456
fabionunesdev <fabionunesbr@gmail.com>
2022-12-20 07:51:46 -0300

    Seção 4 - Criação de View Lanche
    Criação da View para lista os lanches cadastrados.

--- LanchesMac/LanchesMac/Models/Lanche.cs
+++ LanchesMac/LanchesMac/Models/Lanche.cs
@@ -44,10 +44,10 @@ public class Lanche
     [StringLength(200, ErrorMessage = "O {0} deve ter no máximo {1} caracters ")]
     public string ImagemThumbnailUrl { get; set; }
 
-    [Display(Name = "Preferido ?")]
+    [Display(Name = "Preferido")]
     public bool IsLanchePreferido { get; set; }
 
-    [Display(Name = "Em estoque ?")]
+    [Display(Name = "Estoque")]
     public bool EmEstoque { get; set; }
 
     // Relacionamento com Categoria
