7f6a40b7dd6044c958c187a300fd7a1d7b8a71db
fabionunesdev <fabionunesbr@gmail.com>
2023-02-11 08:53:05 -0300

    Seção 6 - Identity: Autenticação e Autorização
    71. Autenticação e Autorização
    72. Configurando o Identity no projeto
    73. Implementando o Login - I
    74. Implementando o Login - II

--- LanchesMac/LanchesMac/Startup.cs
+++ LanchesMac/LanchesMac/Startup.cs
@@ -2,6 +2,7 @@
 using LanchesMac.Models;
 using LanchesMac.Repositories;
 using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace LanchesMac;
@@ -17,6 +18,8 @@ public class Startup
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
 
         services.AddTransient<ILancheRepository, LancheRepository>();
         services.AddTransient<ICategoriaRepository, CategoriaRepository>();
@@ -50,6 +53,9 @@ public class Startup
         app.UseRouting();
        
         app.UseSession();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
         
         app.UseAuthorization();
 
